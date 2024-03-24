d8b6df02e632746a36e2ae0819658c270f5fc608
y0nei <y0nei@proton.me>
2024-03-24 20:24:45 +0100

    fix: Use iwd backend for network-manager
    The default `wpa_supplicant` backend caused wifi freezing issues on my
    machine while the new backend does not.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -23,6 +23,8 @@
 
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
+  networking.wireless.iwd.enable = true;
+  networking.networkmanager.wifi.backend = "iwd";
 
   time.timeZone = "Europe/Warsaw";
 
