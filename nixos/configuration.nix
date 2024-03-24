00d4ad0f04fb204509f2d31aa8c5acc08fb5382b
y0nei <y0nei@proton.me>
2024-03-24 20:20:33 +0100

    feat: Video acceleration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -85,7 +85,21 @@
   programs.sway = {
     enable = true;
     wrapperFeatures.gtk = true;
+  nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override {
+        enableHybridCodec = true;
+    };
   };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; }; # Force intel-media-driver
 
   security.polkit.enable = true;
   services.openssh.enable = true;
