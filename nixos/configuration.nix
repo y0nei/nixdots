550f274de7374a03d39f254940f3e88e10e9fbc8
y0nei <y0nei@proton.me>
2024-04-15 12:24:10 +0200

    fix: Patch for fixing sway workspaces styling
    https://github.com/Alexays/Waybar/issues/3009

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -138,8 +138,16 @@
       grim slurp  # Screenshot utils
       wl-clipboard cliphist  # Clipboard copy/paste
       mako  # Notifications
-      # HACK: Waybar wants Wireplumber 0.4 but unstable has 0.5
-      (waybar.override { wireplumberSupport = false; })
+      ((waybar.overrideAttrs (_: {
+        patches = [
+          # FIX: Sway workspaces (https://github.com/Alexays/Waybar/issues/3009)
+          (pkgs.fetchpatch {
+            url = "https://github.com/Alexays/Waybar/commit/2ffd9a94a505a2e7e933ea8303f9cf2af33c35fe.patch";
+            hash = "sha256-u87t6zzslk1mzSfi4HQ6zDPFr7qMfsvymTy3HBxVTJQ=";
+          })
+        ];
+      # FIX: Waybar wants Wireplumber 0.4 but unstable has 0.5
+      })).override { wireplumberSupport = false; })
     ];
   };
   xdg.portal = {
