8e0ce5410f3ae88bffd1b477505448392bbd00e1
y0nei <y0nei@proton.me>
2024-04-18 22:59:57 +0200

    feat: Enable TLP and thermald

--- hosts/thinkpaw/configuration.nix
+++ hosts/thinkpaw/configuration.nix
@@ -115,6 +115,21 @@
   # Will be exposed through DBus to programs willing to store secrets.
   services.gnome.gnome-keyring.enable = true;
 
+  services.thermald.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
+    };
+  };
   services.upower = {
     enable = true;
     criticalPowerAction = "Hibernate";
