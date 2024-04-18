fd024df8afde8beb1e58c9f97f408cec7c62c34f
y0nei <y0nei@proton.me>
2024-04-18 16:55:53 +0200

    feat: Catppuccin theme for grub

--- hosts/thinkpaw/boot.nix
+++ hosts/thinkpaw/boot.nix
@@ -12,6 +12,17 @@
       efiSupport = true;
       enableCryptodisk = true;
       device = "nodev";
+      theme = pkgs.stdenv.mkDerivation {
+        pname = "catppuccin-grub";
+        version = "1.0";
+        src = pkgs.fetchFromGitHub {
+          owner = "catppuccin";
+          repo = "grub";
+          rev = "803c5df";
+          sha256 = "1kgcs7dl83iwfn6q18b8frikv159an9vpx2kwacn1w2s5faaid7x";
+        };
+        installPhase = "cp -r src/catppuccin-mocha-grub-theme $out";
+      };
     };
   };
 }
